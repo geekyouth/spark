97c218806267f8d76dc58e29f2c28ce9f5fb2d5a
Geek <forsupergeeker@gmail.com>
2021-06-08 20:22:03 +0800

    排除循环依赖

--- sql/core/src/test/scala/org/apache/spark/sql/FromJsonTest.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/FromJsonTest.scala
@@ -12,12 +12,12 @@ import org.apache.spark.sql.test.SharedSparkSession
  * from_json 测试类运行准备：
  * 1. git checkout v3.1.2
  * 2. 在项目路径下打开 git cmd，执行命令： ./build/spark-build-info ./core/target/extra-resources 3.1.2
- * https://stackoverflow.com/a/44416809/9633499
+ *    https://stackoverflow.com/a/44416809/9633499
  * 3. maven --> spark-catalyst --> 右键 generate sources and update folders
- * https://stackoverflow.com/a/63202594/9633499
+ *    https://stackoverflow.com/a/63202594/9633499
  *
  * 参考文档： spark 发行说明：https://spark.apache.org/news/index.html
- * https://spark.apache.org/releases/spark-release-3-0-0.html
+ *    https://spark.apache.org/releases/spark-release-3-0-0.html
  */
 class FromJsonTest extends QueryTest with SharedSparkSession {
   
@@ -29,7 +29,49 @@ class FromJsonTest extends QueryTest with SharedSparkSession {
     """{"a_double":0.012,"b_int":456,"c_bool":true,"d_string":"world","e_long":123456789022}"""
   )
   
-  private[this] val SCHEMA = """{"type":"record","name":"schema","fields":[{"name":"a_double","type":["double"]},{"name":"b_int","type":["int"]},{"name":"c_bool","type":["boolean"]},{"name":"d_string","type":["null","string"],"default":null},{"name":"e_long","type":["null","long"],"default":null}]}"""
+  private[this] val SCHEMA =
+    """
+        |{
+        |  "type": "record",
+        |  "name": "schema",
+        |  "fields": [
+        |    {
+        |      "name": "a_double",
+        |      "type": [
+        |        "double"
+        |      ]
+        |    },
+        |    {
+        |      "name": "b_int",
+        |      "type": [
+        |        "int"
+        |      ]
+        |    },
+        |    {
+        |      "name": "c_bool",
+        |      "type": [
+        |        "boolean"
+        |      ]
+        |    },
+        |    {
+        |      "name": "d_string",
+        |      "type": [
+        |        "null",
+        |        "string"
+        |      ],
+        |      "default": null
+        |    },
+        |    {
+        |      "name": "e_long",
+        |      "type": [
+        |        "null",
+        |        "long"
+        |      ],
+        |      "default": null
+        |    }
+        |  ]
+        |}
+        |""".stripMargin
   
   /**
    * 运行后异常提示转换出错
